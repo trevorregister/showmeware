55fe64a9afc2645ce0590a6d026f0060001964ca
Trevor Register <79274467+trevorregister@users.noreply.github.com>
2024-09-24 12:00:00 -0400

    feat: updated theme

--- src/presentation/plugins/vuetify.js
+++ src/presentation/plugins/vuetify.js
@@ -8,12 +8,26 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
+
 // Composables
 import { createVuetify } from 'vuetify'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: 'customTheme',
+    themes: {
+      customTheme: {
+        dark: false,
+        colors: {
+          primary: '#3D315B',
+          'primary-darken-1': '#2C2341',
+          secondary: '#F5E3E0',
+          accent: '#006D77',
+          background: '#E8B4BC',
+          'secondary-darken-1': '#D89AA5',
+        },
+      },
+    },
   },
 })
