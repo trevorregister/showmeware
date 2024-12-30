b308257207d2b63030be4c26cdcf8e85c2109ccb
Trevor Register <79274467+trevorregister@users.noreply.github.com>
2024-12-30 15:11:34 -0500

    undo profile creation, change to id vs user_id

--- src/infrastructure/repositories/ProfileRepo.js
+++ src/infrastructure/repositories/ProfileRepo.js
@@ -15,29 +15,17 @@ const ProfileRepo = {
         return data
     },
 
-    async updateCalendarId({user_id, calendar_id}) {
+    async updateCalendarId({id, calendar_id}) {
             const { data, error } = await supabase
                 .from('profiles')
                 .update({ calendar_id: calendar_id })
-                .eq('user_id', user_id)
+                .eq('id', id)
         if (error) {
             throw new Error(error.message)
         }
 
         return data
     },
-
-    async createProfile(user_id){
-      const newProfile = {
-          user_id: user_id
-      }
-
-      const { data, error } = await supabase
-          .from('entries')
-          .insert(newProfile)
-
-      return data
-  },
 }
 
 export default ProfileRepo
